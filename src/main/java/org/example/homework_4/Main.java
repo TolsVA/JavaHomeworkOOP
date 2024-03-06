bb35bb41f24605f7d6eb3e1101858313f4ca2f3d
TolsVA <himtol@yandex.ru>
2024-03-06 17:48:11 +0700

    add Node в GbLinkedList

--- src/main/java/org/example/homework_4/Main.java
+++ src/main/java/org/example/homework_4/Main.java
@@ -3,46 +3,39 @@ package org.example.homework_4;
 import org.example.homework_4.lists.GbArrayList;
 import org.example.homework_4.lists.GbLinkedList;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         GbList<Integer> listLinked = new GbLinkedList<>();
         listLinked.add(12);
         listLinked.add(55);
+        listLinked.add(88);
         System.out.println(listLinked);
+        for (Integer integer : listLinked) {
+            System.out.println(integer);
+        }
 
-
-
+        List<Integer> arrayList = new LinkedList<>();
 
 
         GbList<Integer> list = new GbArrayList<>();
         list.add(5);
-        System.out.println("capacity: = " + ((GbArrayList)list).capacity);
         list.add(9);
-        System.out.println("capacity: = " + ((GbArrayList)list).capacity);
         list.add(66);
-        System.out.println("capacity: = " + ((GbArrayList)list).capacity);
         list.add(8);
-        System.out.println("capacity: = " + ((GbArrayList)list).capacity);
         list.add(5);
-        System.out.println("capacity: = " + ((GbArrayList)list).capacity);
         list.add(9);
-        System.out.println("capacity: = " + ((GbArrayList)list).capacity);
         list.add(66);
-        System.out.println("capacity: = " + ((GbArrayList)list).capacity);
         list.add(8);
-        System.out.println("capacity: = " + ((GbArrayList)list).capacity);
         list.add(5);
-        System.out.println("capacity: = " + ((GbArrayList)list).capacity);
         list.add(9);
-        System.out.println("capacity: = " + ((GbArrayList)list).capacity);
         list.add(66);
-        System.out.println("capacity: = " + ((GbArrayList)list).capacity);
         list.add(8);
-        System.out.println("capacity: = " + ((GbArrayList)list).capacity);
-        System.out.println("size: = " + list.size());
         System.out.println(list);
         list.removeByIndex(1);
-        System.out.println("capacity: = " + ((GbArrayList)list).capacity);
         list.remove(66);
         System.out.println("size: = " + list.size());
         System.out.println("capacity: = " + ((GbArrayList)list).capacity);
@@ -51,11 +44,6 @@ public class Main {
         list.remove(66);
         System.out.println(list);
         System.out.println("capacity: = " + ((GbArrayList)list).capacity);
-//        GbList<String> s = new GbArrayList<>();
-//        s.add("asdf");
-//        s.add("xcdf");
-//        s.add("hjdf");
-//        s.add("lkjf");
 
 
 
--- src/main/java/org/example/homework_4/lists/GbLinkedList.java
+++ src/main/java/org/example/homework_4/lists/GbLinkedList.java
@@ -1,20 +1,23 @@
 package org.example.homework_4.lists;
 
 import org.example.homework_4.GbList;
+import org.example.homework_4.lists.util.ArrayIterator;
 
 import java.util.Iterator;
 
 public class GbLinkedList<E> implements GbList<E> {
     // lesson11_2.collections
+    private int size;
+    private int index;
     private Node<E> head;
     private Node<E> tail;
 
-    public class Node<E> {
+    private static class Node<E> {
         E data;
         Node<E> next;
         Node<E> prev;
 
-        public Node(E data, Node<E> next, Node<E> prev) {
+        Node(Node<E> prev, E data, Node<E> next) {
             this.data = data;
             this.next = next;
             this.prev = prev;
@@ -23,16 +26,17 @@ public class GbLinkedList<E> implements GbList<E> {
     @Override
     public void add(E value) {
         if (head == null){
-            head = new Node<>(value,null, null);
+            head = new Node<>(null, value, null);
             tail = head;
         } else {
-            Node<E> node = new Node<>(value, head, tail);
+            Node<E> node = new Node<>(head, value, tail);
             tail.next = node;
             head.prev = node;
             node.prev = tail;
             node.next = head;
             tail = node;
         }
+        size++;
     }
 
     @Override
@@ -58,12 +62,41 @@ public class GbLinkedList<E> implements GbList<E> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public Iterator<E> iterator() {
-        return null;
+        // Здесь масло масленное.
+        // Но цель достигнута прикрутил ArrayIterator
+        E[] list;
+        try {
+            list = (E[]) new Object[size];
+        } catch (ClassCastException e) {
+            throw new RuntimeException(e);
+        }
+        Iterator<E> iterator = new Iterator<>() {
+            private Node<E> node = head;
+            private int index = 0;
+            @Override
+            public boolean hasNext() {
+                return index < size;
+            }
+
+            @Override
+            public E next() {
+                E data = node.data;
+                node = node.next;
+                index++;
+                return data;
+            }
+        };
+        int index = 0;
+        while (iterator.hasNext()){
+            list[index++] = iterator.next();
+        }
+        return new ArrayIterator<E>(list);
     }
 
     @Override
